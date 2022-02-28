4ab37d9fbb7f2373e7cf6071c06795a4eeccff08
jeremy <jeremy@webilo.fr>
2022-02-28 22:24:20 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'bugReproduction',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
